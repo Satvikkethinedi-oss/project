82196195f6a3f19fc04b8baf05733425c7acf81c
Satvikkethinedi-oss <satvikkethinedi@gmail.com>
2025-11-29 19:35:48 +0530

    Updated UI / pages

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,8 +1,14 @@
+import "./AdminDashboard.css";
+import { Link } from "react-router-dom";
 export default function AdminDashboard() {
   return (
     <div style={{ padding: 20 }}>
       <h1>Admin Dashboard</h1>
       <p>Manage all certifications here.</p>
+      <Link to="/users">
+  <button className="btn">Manage Users</button>
+</Link>
+
     </div>
   );
 }
